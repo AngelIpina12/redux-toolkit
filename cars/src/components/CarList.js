12f6ab4d0fe54480b56c7d3495ba0d790ea2420c
AngelIpina12 <angelipina12@hotmail.com>
2025-01-21 16:26:49 -0600

    Add bold highlighting when a car's name is repeated in CarList component

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -5,18 +5,20 @@ import { createSelector } from '@reduxjs/toolkit';
 
 const memoizedCars = createSelector([
   (state) => state.car.cars, (state) => state.car.searchTerm
-], (cars, searchTerm) => cars.filter((car) =>
-  car.name.toLowerCase().includes(searchTerm.toLowerCase())
-));
+], (cars, searchTerm) => {
+  return cars
+    .filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+});
 
 export const CarList = () => {
   const dispatch = useDispatch();
   const cars = useSelector(memoizedCars);
+  const name = useSelector((state) => state.form.name); 
   const handleCarDelete = (car) => dispatch(removeCar(car.id));
-
   const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car.id} className='panel'>
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>{car.name} - {car.cost}</p>
         <button className='button is-danger' onClick={() => handleCarDelete(car)}> Delete</button >
       </div>
