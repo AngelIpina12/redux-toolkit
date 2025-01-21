790952dfbb4160b173cf85e1d665c2dad8d72f4e
AngelIpina12 <angelipina12@hotmail.com>
2025-01-21 12:03:40 -0600

    Add render of cars & handle for delete cars in CarList component

--- cars/src/components/CarForm.js
+++ cars/src/components/CarForm.js
@@ -40,7 +40,7 @@ export const CarForm = () => {
           </div>
         </div>
         <div className='field'>
-          <button className='button is-link'>Submit</button>
+          <button type='submit' className='button is-link'>Submit</button>
         </div>
       </form>
     </div>
--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeCar } from '../store'
 
 export const CarList = () => {
+  const dispatch = useDispatch();
+  const cars = useSelector((state) => state.car.cars);
+  const handleCarDelete = (car) => dispatch(removeCar(car.id));
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className='panel'>
+        <p>{car.name} - {car.cost}</p>
+        <button className='button is-danger' onClick={() => handleCarDelete(car)}> Delete</button >
+      </div>
+    )
+  })
+
   return (
-    <div>CarList</div>
+    <div className='car-list'>
+      {renderedCars}
+      <hr />
+    </div>
   )
 }
