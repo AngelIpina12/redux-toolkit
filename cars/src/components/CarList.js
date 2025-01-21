55260352dbde34539d27bafc0840706efe9ffdc4
AngelIpina12 <angelipina12@hotmail.com>
2025-01-21 13:09:02 -0600

    Add handle for change on searchTerm state in CarSearch component

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { removeCar } from '../store'
+import { createSelector } from '@reduxjs/toolkit';
+
+const memoizedCars = createSelector(
+  [(state) => state.car.cars, (state) => state.car.searchTerm],
+  (cars, searchTerm) =>
+    cars.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+);
 
 export const CarList = () => {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => state.car.cars);
+  const cars = useSelector(memoizedCars);
   const handleCarDelete = (car) => dispatch(removeCar(car.id));
 
   const renderedCars = cars.map((car) => {
