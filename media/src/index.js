176bd89651d7e3a3d7462763d63f60c4ebbeb47e
AngelIpina12 <angelipina12@hotmail.com>
2025-01-22 11:01:00 -0600

    Add completion of installation of dependencies and project preparation

--- media/src/index.js
+++ media/src/index.js
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+import './index.css'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { store } from './store'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const el = document.getElementById('root');
+const root = createRoot(el);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
