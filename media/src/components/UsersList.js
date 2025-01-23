e37fc187d18c87a4c69641b1edc3977148e05d3e
AngelIpina12 <angelipina12@hotmail.com>
2025-01-22 21:42:05 -0600

    Add useState method for creating and creating error states management in UserList component

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -5,32 +5,38 @@ import Skeleton from './Skeleton';
 import Button from './Button';
 
 function UsersList() {
-  const [isLoadingUsers, setIsLoadingUsers] = useState(true);
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
   const [loadingUsersError, setLoadingUsersError] = useState(null);
+  const [isCreatingUsers, setIsCreatingUsers] = useState(false);
+  const [creatingUsersError, setCreatingUsersError] = useState(null);
   const dispatch = useDispatch();
   const { data } = useSelector((state) => {
     return state.users;
   });
 
   useEffect(() => {
-    setIsLoadingUsers(true)
+    setIsLoadingUsers(true);
     dispatch(fetchUsers())
       .unwrap()
       .catch((err) => setLoadingUsersError(err))
-      .finally(() => setIsLoadingUsers(false))
+      .finally(() => setIsLoadingUsers(false));
   }, [dispatch]);
 
   const handleUserAdd = () => {
-    dispatch(addUser());
-  }
+    setIsCreatingUsers(true);
+    dispatch(addUser())
+      .unwrap()
+      .catch((err) => setCreatingUsersError(err))
+      .finally(() => setIsCreatingUsers(false));
+  };
 
   if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full" />;
-  }
+  };
 
   if (loadingUsersError) {
     return <div>Error fetching data...</div>;
-  }
+  };
 
   const renderedUsers = data.map((user) => {
     return (
@@ -45,10 +51,14 @@ function UsersList() {
   return <div>
     <div className="flex flex-row justify-between m-3">
       <h1 className='m-2 text-xl'>Users</h1>
-      <Button onClick={handleUserAdd}>+ Add User</Button>
+      {
+        isCreatingUsers ? 'Creating user...' :
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      }
+      {creatingUsersError && 'Error creating user...'}
     </div>
     {renderedUsers}
-    </div>;
+  </div>;
 }
 
 export default UsersList;
