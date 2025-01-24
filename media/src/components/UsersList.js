fcab22314418a2443368b82e0b26dc82c1237707
AngelIpina12 <angelipina12@hotmail.com>
2025-01-24 10:01:58 -0600

    Add more refactoring in UsersList component

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -15,35 +15,34 @@ function UsersList() {
 
   useEffect(() => { doFetchUsers() }, [doFetchUsers]);
 
+  let content;
   if (isLoadingUsers) {
-    return <Skeleton times={6} className="h-10 w-full" />;
-  };
-
-  if (loadingUsersError) {
-    return <div>Error fetching data...</div>;
-  };
-
-  const renderedUsers = data.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>
+  } else {
+    content = data.map((user) => {
+      return (
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">
+            {user.name}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  };
 
   return <div>
-    <div className="flex flex-row justify-between m-3">
-      <h1 className='m-2 text-xl'>Users</h1>
-      {
-        isLoadingCreate ? 'Creating user...' :
-          <Button loading={isLoadingCreate} onClick={doCreateUsers}>+ Add User</Button>
-      }
-      {loadingCreateError && 'Error creating user...'}
-    </div>
-    {renderedUsers}
-  </div>;
-}
+      <div className="flex flex-row justify-between item-center m-3">
+        <h1 className='m-2 text-xl'>Users</h1>
+        {
+          isLoadingCreate ? 'Creating user...' :
+            <Button loading={isLoadingCreate} onClick={doCreateUsers}>+ Add User</Button>
+        }
+        {loadingCreateError && 'Error creating user...'}
+      </div>
+      {content}
+    </div>;
+  }
 
-export default UsersList;
+  export default UsersList;
